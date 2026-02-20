39f96ee2c2d705a0574bb2e489ed8cd5dff829cd
Milos Faktor <faktor.milos@gmail.com>
2026-02-20 11:29:39 +0100

    testing deployment throught ECR

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          testing branch protection main
+          deployed via ECR
         </p>
         <a
           className="App-link"
