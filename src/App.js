c2ad53c6b6ac169df49c2ef76a39f737537f1678
Milos Faktor <faktor.milos@gmail.com>
2026-02-20 11:47:18 +0100

    testing deployment throught ECR

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          deployed via ECR v1
+          deployed via ECR
         </p>
         <a
           className="App-link"
